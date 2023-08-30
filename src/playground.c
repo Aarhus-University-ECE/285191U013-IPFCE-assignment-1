67fc8107885df24c77e515575d481b21db1dc1ee
kpbaks <kristoffer.pbs@tuta.io>
2023-08-30 15:20:11 +0200

    docs: add comment to explain purpose of `./src/playground.c`

--- src/playground.c
+++ src/playground.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
+// You can use this file to test your code, and experiment with C :D
+
 int main() {
 
-    // printf("Hello, World!\n");
-    // int input;
-    // scanf("%d", &input);
-    
-    return 0;
+  // printf("Hello, World!\n");
+  // int input;
+  // scanf("%d", &input);
+
+  return 0;
 }
